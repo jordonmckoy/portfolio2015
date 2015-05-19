bdcb809f0208caa47a0fb1f54ef52c295b31792d
Jordon <jordonmckoy@gmail.com>
2015-05-19 15:40:46 -0400

    conversion to express complete

---
+++ site/public/scripts/menu.js
@@ -0,0 +1,13 @@
+$("#menu_btn").click(function (event) {
+	event.preventDefault();
+	$('.ghost').each(function () {
+		if ($(this).hasClass('busters') == false) {
+			$(this).addClass('busters');
+			$(this).transition({opacity:'1',visibility:"visible",right:'15px'},800,'ease-in-out');
+		} else {
+			$(this).transition({opacity:'0',visibility:"hidden",right:'-15px'},800,'ease-in-out');
+			$(this).removeClass('busters');
+		}
+	});
+	$('ul.menu').toggleClass('collapsed');
+});
