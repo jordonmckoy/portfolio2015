aa038118b241a10cb198acf893326769644ceae6
Jordon <jordonmckoy@gmail.com>
2015-05-19 22:49:04 -0400

    fixed syntax error in base.jade that prevented the media queries from processing correctly

--- site/public/scripts/menu.js
+++ site/public/scripts/menu.js
@@ -1,13 +1,15 @@
 $("#menu_btn").click(function (event) {
 	event.preventDefault();
 	$('.ghost').each(function () {
-		if ($(this).hasClass('busters') == false) {
+		if ($(this).hasClass('busters') === false) {
 			$(this).addClass('busters');
 			$(this).transition({opacity:'1',visibility:"visible",right:'15px'},800,'ease-in-out');
 		} else {
 			$(this).transition({opacity:'0',visibility:"hidden",right:'-15px'},800,'ease-in-out');
 			$(this).removeClass('busters');
+			$('#menu_btn').blur();
 		}
 	});
 	$('ul.menu').toggleClass('collapsed');
 });
+
